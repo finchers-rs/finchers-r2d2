06688f4388a0cb6bc7b4ec8515cbc90581df8a7e
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-09-30 14:53:14 +0900

    add current_thread

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,81 @@ pub use impl_endpoint::{pool_endpoint, PoolEndpoint};
 #[doc(no_inline)]
 pub use r2d2::*;
 
+#[allow(missing_docs)]
+pub mod current_thread {
+    pub use self::imp::{pool_endpoint, PoolEndpoint};
+
+    mod imp {
+        use finchers::endpoint::{Context, Endpoint, EndpointResult};
+        use finchers::error;
+        use finchers::error::Error;
+
+        use futures::{Async, Future, Poll};
+        use r2d2::{ManageConnection, Pool, PooledConnection};
+        use std::fmt;
+
+        /// Create an endpoint which acquires the connection from the specified connection pool.
+        ///
+        /// This endpoint will block the current thread during retrieving the connection from pool.
+        pub fn pool_endpoint<M>(pool: Pool<M>) -> PoolEndpoint<M>
+        where
+            M: ManageConnection,
+        {
+            PoolEndpoint { pool }
+        }
+
+        #[allow(missing_docs)]
+        #[derive(Debug, Clone)]
+        pub struct PoolEndpoint<M: ManageConnection> {
+            pool: Pool<M>,
+        }
+
+        impl<'a, M> Endpoint<'a> for PoolEndpoint<M>
+        where
+            M: ManageConnection + 'a,
+        {
+            type Output = (PooledConnection<M>,);
+            type Future = PoolFuture<'a, M>;
+
+            fn apply(&'a self, _: &mut Context<'_>) -> EndpointResult<Self::Future> {
+                Ok(PoolFuture { endpoint: self })
+            }
+        }
+
+        pub struct PoolFuture<'a, M: ManageConnection> {
+            endpoint: &'a PoolEndpoint<M>,
+        }
+
+        impl<'a, M> fmt::Debug for PoolFuture<'a, M>
+        where
+            M: ManageConnection + fmt::Debug,
+            M::Connection: fmt::Debug,
+        {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                f.debug_struct("PoolFuture")
+                    .field("endpoint", &self.endpoint)
+                    .finish()
+            }
+        }
+
+        impl<'a, M> Future for PoolFuture<'a, M>
+        where
+            M: ManageConnection,
+        {
+            type Item = (PooledConnection<M>,);
+            type Error = Error;
+
+            fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+                self.endpoint
+                    .pool
+                    .get()
+                    .map_err(error::fail)
+                    .map(|conn| Async::Ready((conn,)))
+            }
+        }
+    }
+}
+
 mod impl_endpoint {
     use finchers::endpoint::{Context, Endpoint, EndpointResult};
     use finchers::error;
