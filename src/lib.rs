361f702d91cd70318331baf334294809be041aa9
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-10-17 17:44:18 +0900

    (cargo-release) bump version to 0.1.2

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 
 //! Generic connection pooling support for Finchers, based on r2d2.
 
-#![doc(html_root_url = "https://docs.rs/finchers-r2d2/0.1.1")]
+#![doc(html_root_url = "https://docs.rs/finchers-r2d2/0.1.2")]
 #![warn(
     missing_docs,
     missing_debug_implementations,
